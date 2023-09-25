fd7051041a662dbeedf7da2681fa3fd78acd1eb1
Maxim Zhvakin <faulpelz1993@gmail.com>
2023-09-25 16:34:54 +0300

    Исправлено написание макроса в заголовочном файле

--- ClassLeaver/ClassLeaver.h
+++ ClassLeaver/ClassLeaver.h
@@ -1,13 +1,13 @@
 #pragma once
-#ifdef CLASSLEAVER_EXPORT
-	#define CLASSLEAVER_API __declspec(dllexport)
+#ifdef leaver_EXPORT
+	#define leaver_API __declspec(dllexport)
 #else
-	#define CLASSLEAVER_API __declspec(dllimport)
-#endif // CLASSLEAVER_EXPORT
+	#define leaver_API __declspec(dllimport)
+#endif // leaver_EXPORT
 
 #include <string>
 
 class Leaver {
 public:
-	CLASSLEAVER_API std::string leave(std::string name);
+	leaver_API std::string leave(std::string name);
 };
